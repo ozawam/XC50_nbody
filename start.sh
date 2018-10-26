9af370e12b0b1697446e31b381165ca97adcf3ed
ozawam <ozawa_m@eps.s.u-tokyo.ac.jp>
2018-10-26 21:32:35 +0900

    first

---
+++ start.sh
@@ -0,0 +1,13 @@
+#PBS -N sample
+#PBS -l nodes=2
+#PBS -q bulk-b
+#PBS -j oe
+#PBS -M ozawa_m@eps.s.u-tokyo.ac.jp
+#PBS -me
+#PBS -r n
+
+cd $PBS_O_WORKDIR
+
+#export OMP_NUM_THREADS=4
+#export OMP_STACKSIZE=512M
+aprun -n 8 -N 4  -d ${OMP_NUM_THREADS}  ./nbody.out -N 10000
